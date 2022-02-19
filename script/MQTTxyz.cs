5f1c2097073472966075737bfc374d76ee948291
aliou <51207@etu.he2b.be>
2022-02-19 15:37:21 +0100

    Json deserilize

--- script/MQTTxyz.cs
+++ script/MQTTxyz.cs
@@ -19,6 +19,30 @@ public class MQTTxyz : MonoBehaviour
 
     }
 
+      public void deserilize_position(Tag tag, Collection<JsonPozyxTags.Example> todo)
+        {
+            foreach (var item in todo)
+            {
+
+                tag.TaginX1 = item.data.coordinates.x;
+                tag.TaginY1 = item.data.coordinates.y;
+                tag.TaginZ1 = item.data.coordinates.z;
+            }
+        }
+            public void deserilize_valueGiroscope(Tag tag, string value, Collection<JsonPozyxTags.Example> todo)
+        {
+
+            foreach (var item in todo)
+            {
+
+                Double[] Giroscopevalue = JsonSerializer.Deserialize<Double[]>(item.data.tagData.sensors[0].value.ToString());
+
+                tag.TagOrientX1 = Giroscopevalue[0];
+                tag.TagOrientY1 = Giroscopevalue[1];
+                tag.TagOrientZ1 = Giroscopevalue[2];
+            }
+        }
+
     public  void mqttmethods()
     {
 
@@ -66,7 +90,7 @@ public class MQTTxyz : MonoBehaviour
     }
     static void clientreceptionMsg(object sender, MqttMsgPublishEventArgs e)
     {//  utf8Encoding = initialise une nouvelle instance de classe a
-     Debug.Log("Message reçue :  " + "  " + System.Text.Encoding.Default.GetString(e.Message));
+     //Debug.Log("Message reï¿½ue :  " + "  " + System.Text.Encoding.Default.GetString(e.Message));
 
         //string source = "TESTMQTT";
       /*  FileStream filemanip = new FileStream(source, FileMode.OpenOrCreate, FileAccess.Write);
@@ -75,6 +99,109 @@ public class MQTTxyz : MonoBehaviour
         var rr = msg;
         writertext.WriteLine(rr);
         writertext.Close();*/
+
+        string path = @"C:\Users\aliou\OneDrive\Bureau\fichier_bureau_etudes.txt";
+            if (File.Exists(path))
+            {//si le fichier existe
+
+                // Create a file to write to.
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    string s = System.Text.Encoding.Default.GetString(e.Message);
+
+                    sw.WriteLine(s);
+
+                }
+            }
+
+
+            // Open the file to read from.
+            using (StreamReader sr = File.OpenText(path))
+            {
+                string s;
+                while ((s = sr.ReadLine()) != null)
+                {
+
+                    //string text = System.Text.Encoding.Default.GetString(e.Message);
+                    Collection<JsonPozyxTags.Example> todo = JsonSerializer.Deserialize<Collection<JsonPozyxTags.Example>>(File.ReadAllText(path));
+                    //	File.ReadAllText(path): lit tout le texte qui se trouve dans le fichier et le ferme
+                    Tag tag = new Tag();
+                    Program p = new Program();
+
+                    foreach (var item in todo)
+                    {
+
+                      
+
+                            if (item.data.coordinates is null && item.data.tagData.sensors is null)
+                            {
+
+                            }
+                            else
+                            {
+
+
+
+                                if (item.data.coordinates is null && !(item.data.tagData.sensors is null))
+                                {
+                                //  Console.WriteLine(item.data.tagData.sensors[0].value);
+                                
+                                Console.WriteLine(item.data.tagData.sensors[0].name);
+                                Console.WriteLine(item.data.tagData.sensors[0].value);
+                                Console.WriteLine("*****************************************");
+                                /* Double[] Giroscopevalue = JsonSerializer.Deserialize<Double[]>(item.data.tagData.sensors[0].value.ToString());
+                                 tag.TagOrientX1 = Giroscopevalue[0];
+                                 tag.TagOrientY1 = Giroscopevalue[1];
+                                 tag.TagOrientZ1 = Giroscopevalue[2];*/
+                                p.deserilize_valueGiroscope(tag, item.data.tagData.sensors[0].value.ToString(), todo);
+
+                            }
+                               
+
+                                if (!(item.data.coordinates is null) && item.data.tagData.sensors is null)
+                                {
+                                //  Console.WriteLine(item.data.tagData.sensors[0].value);
+
+                                Console.WriteLine("x =" + item.data.coordinates.x + "y=" + item.data.coordinates.y + "z=" + item.data.coordinates.z);
+                                Console.WriteLine("*****************************************");
+                               /* tag.TaginX1 = item.data.coordinates.x;
+                                tag.TaginY1 = item.data.coordinates.y;
+                                tag.TaginZ1 = item.data.coordinates.z;*/
+                                p.deserilize_position(tag, todo);
+                            }
+                               
+
+                                if (!(item.data.coordinates is null) && !(item.data.tagData.sensors is null))
+                                {
+                                    //  Console.WriteLine(item.data.tagData.sensors[0].value);
+                                    Console.WriteLine("x =" + item.data.coordinates.x + "y=" + item.data.coordinates.y + "z=" + item.data.coordinates.z);
+                                    Console.WriteLine(item.data.tagData.sensors[0].name);
+                                    Console.WriteLine(JsonSerializer.Deserialize<Double[]>(item.data.tagData.sensors[0].value.ToString()));
+                                // Double[] Giroscopevalue = JsonSerializer.Deserialize<Double[]>(item.data.tagData.sensors[0].value.ToString());
+                                // tag.TagOrientX1 = Giroscopevalue[0];
+                                //tag.TagOrientY1 = Giroscopevalue[1];
+                                //tag.TagOrientZ1 = Giroscopevalue[2];
+                                p.deserilize_valueGiroscope(tag, item.data.tagData.sensors[0].value.ToString(), todo);
+
+                                Console.WriteLine("*****************************************");
+
+                                /*  tag.TaginX1 = item.data.coordinates.x;
+                                  tag.TaginY1 = item.data.coordinates.y;
+                                  tag.TaginZ1 = item.data.coordinates.z;*/
+                                p.deserilize_position(tag, todo);
+                            }
+                               
+
+                  
+
+
+
+                        }
+
+                      
+                    }
+                }
+            }
 
     }
 }
